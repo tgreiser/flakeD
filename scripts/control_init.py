b48f56bb2478dc2c077430778f2b883f57771cc8
tgreiser <tgreiser@gmail.com>
2014-03-16 19:15:22 -0700

    flaked - 94 	Deleted one control slot, and now control # maps to midi (s1-9 for 	sliders, s10-19 for knobs), removed midi from settings table 	presets now loading correctly

--- scripts/control_init.py
+++ scripts/control_init.py
@@ -6,12 +6,14 @@
 # Make sure the corresponding toggle is enabled in the Execute DAT.
 
 def start():
-	print ("--- Initializing control "+me.name)
+	print ("--- Initializing control "+me.parent().name)
 	slider = op('Slider')
-	mod = op('Modulate')
-	print(slider)
+	mod = op('Modulate/slider1')
+	print("Slider: " + slider.name)
 	slider.panel.u.val = op('sliderPreset')[0].eval()
+	print("Mod: "+mod.path)
 	mod.panel.radio.val = op('modPreset')[0].eval()
+	print("Mod value: "+str(mod.panel.radio.val))
 	return
 
 def create():
